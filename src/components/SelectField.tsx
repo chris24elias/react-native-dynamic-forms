2305afba41481262084b79f43842843a38d9714a
Christopher <chris24elias@gmail.com>
2020-01-02 00:25:34 -0500

    added error caption to selectfield

--- src/components/SelectField.tsx
+++ src/components/SelectField.tsx
@@ -1,18 +1,9 @@
-import React from 'react';
-import {Select} from '@ui-kitten/components';
-import styles from '../constants/styles';
-import {View} from 'react-native';
+import React from "react";
+import { Select, Text } from "@ui-kitten/components";
+import styles from "../constants/styles";
+import { View } from "react-native";
 
-const SelectField = ({
-  data,
-  value,
-  setValue,
-  title,
-  placeholder,
-  error,
-  multiSelect,
-  ...otherProps
-}) => {
+const SelectField = ({ data, value, setValue, title, placeholder, error, multiSelect, ...otherProps }) => {
   return (
     <View style={styles.fieldContainer}>
       <Select
@@ -26,15 +17,31 @@ const SelectField = ({
           }
         }}
         placeholder={placeholder}
-        style={{marginBottom: 10}}
+        style={{ marginBottom: 0 }}
         label={title}
-        status={error ? 'danger' : value ? 'success' : 'basic'}
+        status={error ? "danger" : value ? "success" : "basic"}
         // caption={error}
         // ref={getRef}
         // onSubmitEditing={onSubmitEditing}
         multiSelect={multiSelect}
         {...otherProps}
       />
+      {error && (
+        <Text
+          appearance="hint"
+          category="label"
+          status={error ? "danger" : value ? "success" : "basic"}
+          style={{
+            marginTop: 5,
+            fontWeight: "400",
+            fontSize: 12,
+            lineHeight: 16,
+            fontFamily: "System"
+          }}
+        >
+          {error}
+        </Text>
+      )}
     </View>
   );
 };
